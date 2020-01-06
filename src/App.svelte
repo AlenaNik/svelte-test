18e367e1d199e7ef612483a7c360b0d6e164d92c
AlenaNik <alena.nik.digital@gmail.com>
2020-01-06 16:16:41 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let name;
+	import Comp from './components/component.svelte';
+	let batosha = 'alena2'
+	let inputText = 'test'
+
+	function handleClick() {
+		alert('Hi!')
+	}
+</script>
+
+<main>
+	<h1>Hello {name}! </h1>
+	<button on:click={handleClick}>Press Me</button>
+	<input type="text" bind:value={inputText}>
+	<Comp {batosha}>
+		Hello from App
+	</Comp>
+</main>
+
+<style>
+	/* scoped by default */
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/component.svelte
@@ -0,0 +1,16 @@
+<script>
+let value = 42;
+export let batosha = 24;
+</script>
+
+<style>
+div {
+    border: 1px solid red;
+}
+</style>
+
+<div>
+<h3>Hello from here</h3>
+<slot />
+<h3>{batosha}</h3>
+</div>
